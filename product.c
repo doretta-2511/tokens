f68df85b6bfadd892c25bf361fa41c10b3e2851a
doretta-2511 <78247086+doretta-2511@users.noreply.github.com>
2021-02-03 18:03:24 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,14 @@
+#include<iostream.h>
+using namespace std;
+
+int main()
+{
+    double firstNumber, secondNumber, productOfTwoNumbers;
+    cout << "Enter two numbers: ";
+    cin >> firstNumber >> secondNumber;
+    productOfTwoNumbers = firstNumber * secondNumber;  
+
+    cout << "Product = " << productOfTwoNumbers;    
+    
+    return 0;
+}
